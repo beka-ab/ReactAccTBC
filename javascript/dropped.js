bf838db19bd9155503612d9126049c7a3f366734
Beka Abuladze <dbeqaabuladze@gmail.com>
2024-07-27 12:08:12 +0400

    feat: add footer, privacy and rules modal

--- javascript/dropped.js
+++ javascript/dropped.js
@@ -54,3 +54,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("resize", closeAllDropdowns);
 });
+////privacy
+const privacyMenu = document.querySelector(".privacy-menu");
+const privacyCloseIcon = document.querySelector(".privacy-menu-close-div");
+const privacyButton = document.querySelector(".privacy-button");
+const overlay = document.querySelector(".overlay");
+
+privacyButton.addEventListener("click", function () {
+  privacyMenu.classList.add("isOpened");
+  overlay.classList.add("isOpened");
+});
+
+privacyCloseIcon.addEventListener("click", function () {
+  privacyMenu.classList.remove("isOpened");
+  overlay.classList.remove("isOpened");
+});
+
+overlay.addEventListener("click", function () {
+  privacyMenu.classList.remove("isOpened");
+  overlay.classList.remove("isOpened");
+});
+//// rules
+const rulesMenu = document.querySelector(".rules-menu");
+const rulesCloseIcon = document.querySelector(".rules-menu-close-div");
+const rulesButton = document.querySelector(".rules-button");
+const rulesOverlay = document.querySelector(".overlay");
+
+rulesButton.addEventListener("click", function () {
+  rulesMenu.classList.add("isOpened");
+  rulesOverlay.classList.add("isOpened");
+});
+
+rulesCloseIcon.addEventListener("click", function () {
+  rulesMenu.classList.remove("isOpened");
+  rulesOverlay.classList.remove("isOpened");
+});
+
+rulesOverlay.addEventListener("click", function () {
+  rulesMenu.classList.remove("isOpened");
+  rulesOverlay.classList.remove("isOpened");
+});
